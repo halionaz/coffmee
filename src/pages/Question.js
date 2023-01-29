d4ce214273d59b6bfa55487916b155b6023b8874
halionaz <samhsj@naver.com>
2023-01-29 01:37:04 -0500

    ✨ View Questionary

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -2,10 +2,11 @@
 
 import Btn from "../components/Btn"
 
-const Question = ({ stage, setStage }) => {
+const Question = ({ stage, setStage, questions }) => {
     return (
         <div>
             <h2>{stage}번 질문입니다.</h2>
+            <p>{questions[stage - 1].query}</p>
             <Btn text={"다음"} setStage={setStage}/>
             <Btn text={"뒤로가기"} setStage={setStage} diff={-1} />
         </div>
