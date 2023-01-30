a0efbfe27a30384598f2009edb0d7333b5d19190
halionaz <samhsj@naver.com>
2023-01-30 06:14:16 -0500

    ⚡️ Implement weights per question

--- src/components/PointBtn.js
+++ src/components/PointBtn.js
@@ -2,7 +2,15 @@
 
 import style from "../style/PointBtn.module.css";
 
+let weight = 1;
+
 const PointBtn = ({ number, setPoint, quest, setStage }) => {
+
+    if(quest.type){
+        weight = 1;
+    } else {
+        weight = -1;
+    }
     return (
         <div
             className={style.btn}
@@ -13,13 +21,15 @@ const PointBtn = ({ number, setPoint, quest, setStage }) => {
                     });
                     switch (quest.kind) {
                         case "E":
-                            return { ...prev, E: prev.E + number };
+                            return { ...prev, E: prev.E + weight * (number - 3) };
                         case "N":
-                            return { ...prev, N: prev.N + number };
+                            return { ...prev, N: prev.N + weight * (number - 3) };
                         case "T":
-                            return { ...prev, T: prev.T + number };
+                            return { ...prev, T: prev.T + weight * (number - 3) };
                         case "P":
-                            return { ...prev, P: prev.P + number };
+                            return { ...prev, P: prev.P + weight * (number - 3) };
+                        default :
+                            break;
                     }
                 });
             }}
