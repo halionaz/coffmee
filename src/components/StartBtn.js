5689a5041a1cdfa64035fe30eb05b2c27571b2a9
halionaz <samhsj@naver.com>
2023-02-02 06:30:09 -0500

    ✨ Implement BackSpace

--- src/components/StartBtn.js
+++ src/components/StartBtn.js
@@ -1,12 +1,21 @@
 import style from "../style/StartBtn.module.css";
 
-function StartBtn({ text, setStage, diff = 1 }) {
+function StartBtn({ text, setStage, diff = 1, setPoint }) {
     return (
         <div
             className={style.btn}
             onClick={() => {
-                if(diff === -1){
+                if (diff === -1) {
                     // 뒤로 가기임
+                    setPoint((prev) => {
+                        const newPoint = [];
+                        for(let i = 0 ; i < prev.length; i++){
+                            if(i != prev.length - 1){
+                                newPoint.push(prev[i]);
+                            }
+                        }
+                        return newPoint;
+                    });
                 }
                 setStage((prev) => {
                     return prev + diff;
