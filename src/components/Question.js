e75944c3942ab9036baf817050d2d1855c40e197
halionaz <samhsj@naver.com>
2023-03-04 03:51:20 -0500

    🎨 Implement Basic Layout of Question Page

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,19 +2,22 @@
 
 import PointBtn from "./PointBtn";
 import Btn from "./BackBtn";
+import style from "../style/Question.module.css";
 
 const pointList = [1, 2, 3, 4, 5];
 
 const Question = ({ stage, setStage, quest, setPoint }) => {
     return (
-        <div>
-            <h2>{stage + 1}</h2>
+        <div className={style.Question}>
+            <h2 className={style.Numbering}>{stage + 1}</h2>
             <p>{quest.query}</p>
-            {pointList.map((num) => {
-                return (
-                    <PointBtn key={num} number={num} setPoint={setPoint} quest={quest} setStage={setStage} />
-                );
-            })}
+            <div className={style.numbers}>
+                {pointList.map((num) => {
+                    return (
+                        <PointBtn key={num} number={num} setPoint={setPoint} quest={quest} setStage={setStage} />
+                    );
+                })}
+            </div>
             {stage !== 0 && <Btn text={"뒤로가기"} setStage={setStage} setPoint={setPoint} diff={-1} />}
         </div>
     );
