3c083bc354e7536d3a542efd74a3ddadcffd2770
halionaz <samhsj@naver.com>
2023-03-05 02:46:16 -0500

    🚚 Change Img Directory

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,6 +1,5 @@
 // 랜딩 페이지
 import StartBtn from "../components/StartBtn";
-import emptyCup from "../img/emptyCup.png";
 import style from "../style/Start.module.css";
 
 const Start = () => {
@@ -13,7 +12,7 @@ const Start = () => {
                 </h1>
                 <div className={style.text}>당신은 어떤 커피와 어울리나요?</div>
             </div>
-            <img src={emptyCup} className={style.logo} alt="cup img logo" />
+            <img src={"/img/emptyCup.png"} className={style.logo} alt="cup img logo" />
             <div className={style.btnFlex}>
                 <StartBtn text={`시작하기`}></StartBtn>
                 <div className={style.share}>친구에게 공유하기</div>
