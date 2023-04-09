43af32595ad0c2b48b127179b164637d8dec8361
halionaz <samhsj@naver.com>
2023-04-09 03:44:13 -0400

    🐛 Fix Google Analytics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import Start from "./pages/Start";
 import Result from "./pages/Result";
 import Questionary from "./pages/Questionary";
-import RouteChangeTracker from "./RouteChangeTracker";
 import "./App.css";
 
 const App = () => {
-    RouteChangeTracker();
     return (
         <HashRouter>
             <div className="App">
